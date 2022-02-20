178a1198084a0d38f291f585430e4bf6d4987040
branko-bajic <branko@mailfence.com>
2022-02-20 16:55:44 -0500

    Added alient encounter font, updated background image, fixed flex layout

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import './App.css';
 import cloud from './assets/fluffy-cloud.png';
 import background from './assets/neon-city.jpg';
+import { TransitionGroup } from 'react-transition-group';
+
 
 function App() {
   return (
     <div className="App" style={{ backgroundImage: `url(${background})` }}>
-      <div className="App-header">
-        Branko Bajic
+      <div className="Clouds-Section">
+        <img 
+          src={cloud} 
+          className="Cloud" 
+          alt="cloud" 
+          height="60%"
+          width="auto"
+        />
       </div>
-      <div className="App-footer">
+      <div className="Header">
         Branko Bajic
+        <br/>
+        Full Stack Software Developer
       </div>
+      <div className="Body" />
+      <div className="Footer" />
     </div>
   );
 }
