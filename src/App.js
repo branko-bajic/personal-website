debbeba12c5e86a18a95216e40bb8c545fbc0b1c
branko-bajic <branko@mailfence.com>
2022-02-22 15:57:01 -0500

    Added new css pixel art and initial animations for clouds

--- src/App.js
+++ src/App.js
@@ -2,19 +2,27 @@ import './App.css';
 import cloud from './assets/fluffy-cloud.png';
 import background from './assets/neon-city.jpg';
 import { TransitionGroup } from 'react-transition-group';
+import './Clouds.css';
 
 
 function App() {
   return (
     <div className="App" style={{ backgroundImage: `url(${background})` }}>
       <div className="Clouds-Section">
-        <img 
+        <div className="Cloud">
+          <div className="shadowed-cloud"/>
+        </div>
+        <div className="Cloud">
+          <div className="cloud"/>
+        </div>
+
+        {/* <img 
           src={cloud} 
           className="Cloud" 
           alt="cloud" 
           height="60%"
           width="auto"
-        />
+        /> */}
       </div>
       <div className="Header">
         Branko Bajic
