b88debf992710e3ddbb65c4ea448bea3d1b6a1f5
Branko Bajic <brankothebajic@gmail.com>
2022-03-09 01:16:59 -0700

    Clean up cloud bounds and add menu content

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
+import React, { useState } from 'react';
 import './App.css';
 import background from './assets/neon-city.jpg';
 import './Clouds.css';
 
 
 function App() {
+  const MAINMENU = 0;
+  const PROJECTS = 1;
+  const CONTACT = 2;
+  const [optionSelected, setOptionSelected] = React.useState(MAINMENU);
   return (
     <div className="App" style={{ backgroundImage: `url(${background})` }}>
       <div className="Clouds-Section">
+
+        {/* Shadowed Cloud */}
         <div className="shadowed-cloud-container">
           <div className="shadowed-cloud-sprite"/>
         </div>
+
+        {/* Regular Cloud */}
         <div className="cloud-container">
           <div className="cloud-sprite"/>
         </div>
+
+        {/* Small Cloud */}
         <div className="small-cloud-container">
           <div className="small-cloud-sprite"/>
         </div>
@@ -23,13 +34,51 @@ function App() {
         Full Stack Software Developer
       </div>
       <div className="Body">
-        <div className="selectable-option">
-          {"> Projects"}
-        </div>
-        <br/>
-        <div className="selectable-option">
-          {"> Contact"}
-        </div>
+
+        {/* Main Menu */}
+        {optionSelected === MAINMENU &&
+          <React.Fragment>
+            <div className="selectable-option" onMouseUp={() => setOptionSelected(PROJECTS)}>
+              {"> Projects <"}
+            </div>
+            <br/>
+            <div className="selectable-option" onMouseUp={() => setOptionSelected(CONTACT)}>
+              {"> Contact <"}
+            </div>
+          </React.Fragment>
+          }
+
+        {/* Projects */}
+        {optionSelected === PROJECTS &&
+          <React.Fragment>
+            <div className="selectable-option">
+              {"> OSRS Castaway <"}
+            </div>
+            <br/>
+            <br/>
+            <div className="selectable-option" onMouseUp={() => setOptionSelected(MAINMENU)}>
+              {"> Back to Main Menu <"}
+            </div>
+          </React.Fragment>
+        }
+
+        {/* Contact */}
+        {optionSelected === CONTACT &&
+          <React.Fragment>
+            <div className="selectable-option">
+              {"> GitHub <"}
+            </div>
+            <br/>
+            <div className="selectable-option">
+              {"> LinkedIn <"}
+            </div>
+            <br/>
+            <br/>
+            <div className="selectable-option" onMouseUp={() => setOptionSelected(MAINMENU)}>
+              {"> Back to Main Menu <"}
+            </div>
+          </React.Fragment>
+        }
       </div>
       <div className="Footer" />
     </div>
